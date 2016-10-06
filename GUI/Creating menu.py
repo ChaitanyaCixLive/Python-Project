cfbdddee6b38f18411eab0515d4f3b6b1abe3d92
Bikash <bikash11roy@gmail.com>
2016-10-06 23:08:25 +0600

    Drop down menu added

---
+++ GUI/Creating menu.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+def doNothing():
+    print("ok do Nothing")
+
+root = Tk()
+
+menu = Menu(root)
+root.config(menu=menu)
+
+subMenu = Menu(menu)
+menu.add_cascade(label="File", menu=subMenu)
+
+subMenu.add_command(label="New Project", command=doNothing)
+subMenu.add_command(label="Open", command=doNothing)
+
+subMenu.add_separator()
+
+subMenu.add_command(label="Exit", command=doNothing)
+
+editMenu = Menu(menu)
+menu.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Redo", command=doNothing)
+
+
+
+root.mainloop()
