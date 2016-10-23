586a5154a429a7abc0d46620c10567da21454e39
Bikash <bikash11roy@gmail.com>
2016-10-23 20:46:34 +0600

    Python basic

---
+++ Basic/Data Structure/Unpacking Element.py
@@ -0,0 +1,20 @@
+records = [
+    ('foo', 4,5),
+    ('goo', 'hello'),
+    ('foo',6,7)
+]
+
+
+def do_foo(x,y):
+    print('foo', x, y)
+
+
+def do_goo(s):
+    print('goo', s)
+
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'goo':
+        do_goo(*args)
