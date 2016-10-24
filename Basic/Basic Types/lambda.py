420e2f454ff3f7130436b82d0477515da5d8adc2
Bikash <bikash11roy@gmail.com>
2016-10-24 23:57:57 +0600

    lambda

---
+++ Basic/Basic Types/lambda.py
@@ -0,0 +1,16 @@
+def increment(n):
+    return lambda x: x + n
+
+
+f = increment(20)
+print(f)
+print(f(5))
+
+
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+print(type(pairs))
+print(type(pairs[0]))
+pairs.sort(key=lambda l: l[1])
+
+print(pairs)
+
