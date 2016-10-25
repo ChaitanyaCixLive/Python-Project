f0b822ef2d08f7b216ed33b1db97fbee504e6108
Bikash <bikash11roy@gmail.com>
2016-10-25 17:27:39 +0600

    String Basic

---
+++ Basic/Data Structure/looping.py
@@ -0,0 +1,10 @@
+tel = {'jack': 4098, 'sape': 4139}
+print(tel)
+
+for u,v in tel.items():
+    print(u,v)
+print()
+
+lst = ['tic', 'tac', 'toe']
+for i, v in enumerate(lst):
+    print(i, v)
---
+++ Basic/input output/string.py
@@ -0,0 +1,8 @@
+hello = 'hello, \nworld'
+hellos = repr(hello)
+print(hellos)
+
+helloStr = str(hello)
+print(helloStr)
+print()
+
