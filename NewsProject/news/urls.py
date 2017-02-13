b5318d3e5a914e7ef9770464488aa599fcbbfe5f
Bikash <bikash11roy@gmail.com>
2017-02-13 21:00:35 +0600

    Form Post

--- NewsProject/news/urls.py
+++ NewsProject/news/urls.py
@@ -5,5 +5,7 @@ from news import views
 
 urlpatterns = [
     url(r'^$', views.articles_list, name='articles_list'),
+    url(r'^feeds/new', views.feed_new, name='feed_new'),
     url(r'^feeds/', views.feeds_list, name='feeds_list'),
+
 ]
--- NewsProject/news/views.py
+++ NewsProject/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Article, Feed
+from .froms import FeedForm
+from django.shortcuts import redirect
 # Create your views here.
 
 
@@ -14,3 +16,18 @@ def articles_list(request):
 def feeds_list(request):
     feeds = Feed.objects.all()
     return render(request, 'news/feeds_list.html', {'feeds': feeds})
+
+
+def feed_new(request):
+    if request.method == "POST":
+        form = FeedForm(request.POST)
+        if form.is_valid():
+            feed = form.save(commit=False)
+            # set some fields
+            feed.title = "Titel"
+            form.save()
+            return redirect('/news/feeds/feeds_list.html')
+    else:
+        form = FeedForm()
+        return render(request, 'news/new_feed.html', {'form': form})
+
