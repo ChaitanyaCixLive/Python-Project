01e4a5095de1af11263d743976115ea12dae626c
Bikash <bikash11roy@gmail.com>
2017-02-13 07:51:52 +0600

    model for news app created, Feed , News

--- NewsProject/NewsProject/settings.py
+++ NewsProject/NewsProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'NewsProject.urls'
+ROOT_URLCONF = 'newsproject.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'NewsProject.wsgi.application'
+WSGI_APPLICATION = 'newsproject.wsgi.application'
 
 
 # Database
