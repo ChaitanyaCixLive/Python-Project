96460f2b4f06e5b9da0b589e1f3269cdd7a573a8
Bikash <bikash11roy@gmail.com>
2017-02-13 06:18:16 +0600

    Django Email Project database added

--- DjangoEmail/src/settings.py
+++ DjangoEmail/src/settings.py
@@ -113,6 +113,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_DIRS = (
+    '/src/templates/',
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
