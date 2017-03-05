27e4eb7456a2c024a0e9ac33395627e4eb4ab072
Bikash <bikash11roy@gmail.com>
2017-03-06 00:36:30 +0600

    auto mated testing added

--- PollSite/polls/tests.py
+++ PollSite/polls/tests.py
@@ -5,8 +5,20 @@ from django.test import TestCase
 from .models import Question
 
 
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date = time)
+
+
 class QuestionMethodTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date = time)
         self.assertIs(future_question.was_published_recently(), False)
+
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_question(self):
+        response = self.client.get(reversed('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
