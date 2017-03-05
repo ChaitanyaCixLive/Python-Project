8033976ae9f51248497271a4609fbd9fdc7ed3d4
Bikash <bikash11roy@gmail.com>
2017-03-06 00:08:36 +0600

    fixing date view bag

--- PollSite/polls/views.py
+++ PollSite/polls/views.py
@@ -1,7 +1,9 @@
+from datetime import timezone
+
 from django.views import generic
 
 from polls.models import Question, Choice
-from django.template import loader
+from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
@@ -13,7 +15,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
