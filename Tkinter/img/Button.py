57da1a69dfd1062dafa9b04ef3456114084b93e9
Bikash <bikash11roy@gmail.com>
2017-03-08 23:48:24 +0600

    button onclick fucntionality

--- Tkinter/img/Button.py
+++ Tkinter/img/Button.py
@@ -1,9 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 
+
+def callback():
+    print('Clicked!')
+
+
 root = Tk()
 
 button = ttk.Button(root, text="Click me")
 button.pack()
+button.config(command=callback)
 
 root.mainloop()
+
+
