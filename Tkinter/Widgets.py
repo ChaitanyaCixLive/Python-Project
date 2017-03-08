50c97f764c2ba20fd47e4c934ab9d85c5e0c5671
Bikash <bikash11roy@gmail.com>
2017-03-08 23:01:42 +0600

    Tkinter image

--- Tkinter/Widgets.py
+++ Tkinter/Widgets.py
@@ -4,4 +4,10 @@ from tkinter import ttk
 root = Tk()
 label = ttk.Label(root, text="Hello Tkinter")
 label.pack()
+label.config(foreground='blue', background='yellow')
+label.config(font=('Courier', 18, 'bold'))
+
+logo = PhotoImage(file="img/python_logo.gif")
+label.config(image = logo)
+
 root.mainloop()
