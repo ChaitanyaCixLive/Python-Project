0f46c9d0e175900b105bf4750fc62de0f533d3db
Bikash <bikash11roy@gmail.com>
2017-03-09 00:18:29 +0600

    Radio Button in tkinter

--- Tkinter/Selection_button.py
+++ Tkinter/Selection_button.py
@@ -6,4 +6,15 @@ root = Tk()
 check_botton = ttk.Checkbutton(root, text='Spam?')
 check_botton.pack()
 
+spam = StringVar()
+
+check_botton.config(variable=spam, onvalue='spam', offvalue='not a spam')
+print(spam.get())
+
+breakfast=StringVar()
+ttk.Radiobutton(root, text='SPAM', variable=breakfast, value="SPAM").pack()
+ttk.Radiobutton(root, text='Eggs', variable=breakfast, value="Eggs").pack()
+ttk.Radiobutton(root, text='Sausage', variable=breakfast, value="Sausage").pack()
+ttk.Radiobutton(root, text='Sausage', variable=breakfast, value="Sausage").pack()
+
 root.mainloop()
