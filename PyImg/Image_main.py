9d3880c1dd941f15855cc2d886b61d57e9b48760
Bikash <bikash11roy@gmail.com>
2017-03-09 18:47:56 +0600

    Cropping image

--- PyImg/Image_main.py
+++ PyImg/Image_main.py
@@ -1,5 +1,14 @@
 from PIL import Image
 
 img = Image.open('python_logo.gif')
-print(img.size)
-img.show()
+
+# Printing size and showing it in viewer
+# print(img.size)
+# img.show()
+
+# Cropping an image
+
+area = (100, 100, 200, 200)     # First two are upper co-ordinate other two are lower coordinate
+cropped_image = img.crop(area)
+
+cropped_image.show()
