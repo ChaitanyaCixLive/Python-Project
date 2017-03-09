20f86d7c79413424d37ed943052688df61beb3d9
Bikash <bikash11roy@gmail.com>
2017-03-10 00:15:40 +0600

    Tkinter text

--- Tkinter/Advance Widget/text.py
+++ Tkinter/Advance Widget/text.py
@@ -13,5 +13,15 @@ text.get('1.0', '1.end')
 # inserting text in specific line
 text.insert('1.0 + 2 lines', 'Inserted text')
 
+# adding tag
+text.tag_add('my_tag', '1.0', '1.0 wordend')
+text.tag_configure('my_tag', background='yellow')
+
+# removing tag from some area
+text.tag_remove('my_tag', '1.1', '1.3')
+
+# deleting a tag
+text.tag_delete('my_tag')
+
 
 root.mainloop()
