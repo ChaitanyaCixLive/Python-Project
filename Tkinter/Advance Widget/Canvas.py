ffd36d4331390d521b7e3571dcf4fb20eb4e443c
Bikash <bikash11roy@gmail.com>
2017-03-12 03:47:35 +0600

    Canvas Image. text, basic shapes

--- Tkinter/Advance Widget/Canvas.py
+++ Tkinter/Advance Widget/Canvas.py
@@ -17,4 +17,18 @@ canvas.itemconfig(line, smooth=True)
 # making it 5 break smooth
 canvas.itemconfig(line, splinesteps=5)
 
+rect = canvas.create_rectangle(160, 120, 480, 360)
+canvas.itemconfig(rect, fill='yellow')
+
+oval = canvas.create_oval(160, 120, 480, 360)
+arc = canvas.create_arc(160, 1, 480, 240)
+canvas.itemconfig(arc, start=0, extent=180, fill='green')
+
+poly = canvas.create_polygon(160,360,320,480,480,360, fill='blue')
+text = canvas.create_text(320, 240, text='Python', font=('Courier', 32, 'bold'))
+
+logo = PhotoImage(file='img/python_logo.gif')
+image = canvas.create_image(320, 240, image=logo)
+canvas.lift(text)
+
 root.mainloop()
