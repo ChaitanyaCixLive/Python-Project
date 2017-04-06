c92f95ceb0f7545bea65bd5360330da34a23303c
Bikash <bikash11roy@gmail.com>
2017-04-07 00:47:24 +0600

    Auto reload and debug on run

--- Bottle/app.py
+++ Bottle/app.py
@@ -5,4 +5,14 @@ from bottle import run, route
 def index():
     return '<h1>Hello world</h1>'
 
-run(host='localhost', port=8082)
+
+@route('/login')
+def login():
+    return '<h1>Log in page</h1>'
+
+
+@route('/reg')
+def login():
+    return '<h1>Registration page</h1>'
+
+run(host='localhost', port=8082, debug=True, reloader=True)
