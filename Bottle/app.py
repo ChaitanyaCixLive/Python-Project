b7e4cb35c4f225ba6e94afe644eb729d2f69d70c
Bikash <bikash11roy@gmail.com>
2017-04-07 01:20:39 +0600

    POST GET method

--- Bottle/app.py
+++ Bottle/app.py
@@ -20,4 +20,15 @@ def login():
 def aritcle(id):
     return '<h1> You are viewing article ' + id + '</h1>'
 
+
+@route('/page/<id>/<name>')
+def page(id, name):
+    return '<h1>you are viewing page id '+id+'</h1>'+'<br><p>Written by '+name+'</P>';
+
+
+@route('/posted', method='POST')
+def posted():
+    return '<h1>Post</h1>'
+
+
 run(host='localhost', port=8082, debug=True, reloader=True)
