18c8db3bc67d4d63e35e0b910cf21e54ee8b8323
Bikash <bikash11roy@gmail.com>
2017-04-07 01:50:41 +0600

    Passing data to view

--- Bottle/app.py
+++ Bottle/app.py
@@ -3,7 +3,7 @@ from bottle import run, route, template
 
 @route('/')
 def index():
-    return template('index')
+    return template('index', name="Bikash")
 
 
 @route('/login')
--- Bottle/views/index.tpl
+++ Bottle/views/index.tpl
@@ -1,4 +1,4 @@
-<h1>Hello Bikash</h1>
+<h1>Hello {{name}}</h1>
 
 % if True:
     <h1>If block</h1>
