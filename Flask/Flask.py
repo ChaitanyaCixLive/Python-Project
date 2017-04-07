37d83aba1f1f5304d9fccf9b718f280ba3391ee9
Bikash <bikash11roy@gmail.com>
2017-04-07 20:03:23 +0600

    Passing parameter

--- Flask/Flask.py
+++ Flask/Flask.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return "<h1>this is post with id "+str(post_id)+"</h2>"
+
 
 if __name__ == '__main__':
     app.run()
